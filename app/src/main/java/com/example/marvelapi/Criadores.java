cca50bf802dab2c8c4eb5a9a66368cb14b113544
DaImata <65667134+DaImata@users.noreply.github.com>
2020-07-18 16:39:02 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/marvelapi/Criadores.java
@@ -0,0 +1,19 @@
+package com.example.marvelapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Criadores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criadores);
+        setTitle("Criadores Marvel");
+    }
+    public void pesquisarCriadores (View view){
+
+    }
+}
---
+++ app/src/main/java/com/example/marvelapi/Quadrinhos.java
@@ -0,0 +1,19 @@
+package com.example.marvelapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Quadrinhos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadrinhos);
+        setTitle("Quadrinhos Marvel");
+    }
+    public void pesquisarQuadrinhos (View view){
+
+    }
+}
